b0275ec981e78b2f247bd80f085e64442e6fbd3f
bhanurawal <81429396+bhanurawal@users.noreply.github.com>
2025-01-05 14:33:41 +0530

    Added chunking code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,35 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def split_text(documents: list[Document]):
+  """
+  Split the text content of the given list of Document objects into smaller chunks.
+  Args:
+    documents (list[Document]): List of Document objects containing text content to split.
+  Returns:
+    list[Document]: List of Document objects representing the split text chunks.
+  """
+  # Initialize text splitter with specified parameters
+  text_splitter = RecursiveCharacterTextSplitter(
+     chunk_size=500, # Size of each chunk in characters
+     chunk_overlap=100, # Overlap between consecutive chunks
+     length_function=len, # Function to compute the length of the text
+     add_start_index=True, # Flag to add start index to each chunk
+   )
+
+  #  text_splitter = SemanticChunker(lc_embed_model)
+
+  # Split documents into smaller chunks using text splitter
+  chunks = text_splitter.split_documents(documents)
+  print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+  # Print example of page content and metadata for a chunk
+  document = chunks[0]
+  print(document.page_content)
+  print(document.metadata)
+
+  return chunks # Return the list of split text chunks
 
 def main():
     st.title("File Upload and Q&A App")
@@ -10,7 +40,8 @@ def main():
         st.write("File uploaded successfully!")
         # Add your RAG model processing code here
         binary_data = uploaded_file.getvalue()
-        pdf_viewer(input=binary_data, width=700)
+        # pdf_viewer(input=binary_data, width=700)
+        chunks = split_text(binary_data)
         
         question = st.text_input("Ask a question about the file")
         
